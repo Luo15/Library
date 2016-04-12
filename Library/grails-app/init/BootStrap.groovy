23d6c7e3fb530bde0bc348e5b3d6c5b3f5a8e15b
todza66 <t.d.maswera@gmail.com>
2016-04-12 12:32:22 +0100

    Added three details in BootStrap.groovy which is Course, Student and Library

--- Library/grails-app/init/BootStrap.groovy
+++ Library/grails-app/init/BootStrap.groovy
@@ -1,7 +1,20 @@
+import com.Library.*
 class BootStrap {
 
     def init = { servletContext ->
-             
+             def library1=new Library(location:'SHU university',
+                                     openingHours:'24 Hours').save()
+
+             def student1=new Student(name:'Tom',
+                                      email:'b5450758@my.shu.ac.uk',
+                                      studentid:'B5450758',
+                                      course:'Computing').save()
+
+             def course1=new Course(title:'E-business',
+                                    code:'WA234',
+                                    tutor:'Inze Haire',
+                                    department:'Computing',
+                                    description:'''Lorem ipsum dodlr sit amet, ad sea solum brute sensibus, te summo seneserit ei.''').save()
     }
     def destroy = {
     }
