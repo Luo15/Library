4243bae7d0e177f55ab10d20941310e7b59eeb3c
todza66 <t.d.maswera@gmail.com>
2016-04-14 15:58:41 +0100

    Delete all Html and login code

--- Library/grails-app/controllers/com/Library/LibrarianController.groovy
+++ Library/grails-app/controllers/com/Library/LibrarianController.groovy
@@ -4,41 +4,6 @@ class LibrarianController {
 
     def scaffold = Librarian 
         
-        def login(){
-        if(params.cName)
-
-        return [cName:params.cName,aName:params.aName]
-}
-
-    def validate(){
-     
-        def user = Librarian.findByUserName(params.username)
         
-        if(user && user.password == params.password){
-         
-          session.user = user
-
-          if(params.cName)
-
-            redirect controller:params.cName,action:params.aName
-
-          else
-
-            redirect controller:'library',action:'index'}
-   
-          else{
-
-            flash.message = "invalid username and password."
-
-            render view:'login'
-}
-}
-
-    def logout = {
- 
-          session.user = null
-
-          redirect(url:'/')}
-
    }
-}
+
